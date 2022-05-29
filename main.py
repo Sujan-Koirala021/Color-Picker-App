e95b390ddad89191e9e0264d1301baa7579e0740
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2022-05-29 15:15:29 +0545

    Copy to clipboard success

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from textwrap import wrap
+#   Color Picker
+#   Picks hex code for different rgb values
+#   May 29, 2022
+
 from tkinter import *
+from tkinter import messagebox
 
 
 root = Tk()
 root.title("Color picker")
 root.geometry('560x300')
 
+#   Disable maximize
+root.resizable(False, False)
+
+
 def slide(value):
     r = redValue.get()
     g = greenValue.get()
     b = blueValue.get()
     rgb = (r, g, b)
+    
     #   Convert (r, g, b) into hex code
     hexcode = "#%02x%02x%02x" % rgb
     
@@ -21,6 +30,19 @@ def slide(value):
     hexDisplay.delete(1.0, END)    
     hexDisplay.insert(END, hexcode)
     
+    
+def copySuccess():
+    messagebox.showinfo("Copy Success", "Copied Successfully!!")    #   Show success message box
+    clip = Tk()                                                     #   Create small window
+    clip.withdraw()                                                 #   Withdraw value
+    clip.clipboard_clear()                                          #   Clear clipboard in Tk clipboard
+    clip.clipboard_append(hexDisplay.get(1.0, END))                 #   Get hexcode copied to clipboard
+    clip.destroy()                                                  #   Destroy this and all descendent widgets
+    
+    #   Now you can paste it anywhere
+
+
+# Holds integer data where we can set integer data and can retrieve it as well using getter and setter methods.
 redValue = IntVar()
 greenValue = IntVar()
 blueValue = IntVar()
@@ -30,7 +52,8 @@ blueValue = IntVar()
 colorFrame = Label(root, bg = "black", width=40, height = 15)
 colorFrame.place(x = 10, y = 10)
 
-#   Making color scales
+#   Making color scales(sliders)
+
 #   Red
 redSlider = Scale(root, variable = redValue,fg = "red", from_=0, to=255,length = 200, cursor = "dot", highlightbackground = "red",orient = "horizontal", sliderlength=20, label="Red", command = slide)
 redSlider.place(x = 330, y= 20)
@@ -43,15 +66,18 @@ greenSlider.place(x = 330, y= 120)
 blueSlider = Scale(root, variable = blueValue, fg = "blue", from_=0, to=255,length = 200, cursor = "dot", highlightbackground = "blue",orient = "horizontal", sliderlength=20, label="Blue", command = slide)
 blueSlider.place(x = 330, y= 220)
 
-#   Hex Code
+#   Hex  Label
 hexLabel = Label(root, text = "Hex Code : ")
 hexLabel.place(x=10, y = 270)
 
-#   Hex Code text box
+#   Hex Code Text box
 hexDisplay = Text(root, height = 1, width = 10, wrap = WORD)
 hexDisplay.place(x = 80, y = 270)
 hexDisplay.insert(END, "#000000")
-#   Disable maximize
-root.resizable(False, False)
+
+#   Copy Button
+copyButton = Button(root, text = "Copy", command = copySuccess)
+copyButton.place(x = 200, y = 270)
+
 
 root.mainloop()
